a27bc2333c065ac215ccf80354c0acc924952ea9
willndira <willndira@gmail.com>
2016-05-16 12:57:30 +0300

    Validation yet to be done

--- Hospital/app/src/main/java/mspace/com/hospital/RegisterActivity.java
+++ Hospital/app/src/main/java/mspace/com/hospital/RegisterActivity.java
@@ -2,12 +2,21 @@ package mspace.com.hospital;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    EditText etFullnames , etUsername ,etEmail ,etPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        etFullnames = (EditText)findViewById(R.id.eTfullNames);
+        etEmail = (EditText)findViewById(R.id.eTemail);
+        etUsername = (EditText)findViewById(R.id.etUsername);
+        etPassword = (EditText)findViewById(R.id.eTpassword);
+
+
+
     }
 }
